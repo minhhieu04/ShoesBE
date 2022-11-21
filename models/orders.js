c31607d6f352d8a4243a81f18a43754acdf7a6a8
Emma minhhieu04 <minhhieu.tran.mcs@gmail.com>
2022-11-21 18:59:59 +0700

    create orders Schema on models

---
+++ models/orders.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const odersSchema = new Schema(
+    {
+        productId: {
+            type: String,
+            required: true,
+        },
+        productName: {
+            type: String,
+            required: true,
+        },
+        productBrand: {
+            type: String,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: false,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        userId: {
+            type: String,
+            required: true,
+        },
+        userName: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        orderStatus: {
+            type: Boolean,
+            required: true
+        }
+    },   
+    { timestamps: true }
+)
+
+const Orders = mongoose.model('Orders', odersSchema)
+module.exports = Orders
